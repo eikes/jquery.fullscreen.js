533faa3b814e82c92be64a7a9a876c68e4f96f15
Eike Send <eike.send@gmail.com>
2012-06-04 18:42:28 +0200

    fix demo for IE8

--- jquery.fullscreenslides.js
+++ jquery.fullscreenslides.js
@@ -302,6 +302,7 @@
     $container.css({
       "position"         : "absolute",
       "top"              : "0px",
+      "left"             : "0px",
       "width"            : "100%",
       "text-align"       : "center",
       "background-color" : options.bgColor
